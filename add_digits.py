c4e66f92aafe9f5ff9d40597b9f52d72ad9ecbae
Eric Tsai <chunchieh@gmail.com>
2015-09-10 09:38:24 +0800

    Add method: congruence_formula

--- add_digits.py
+++ add_digits.py
@@ -5,10 +5,16 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        result = num
+        return self.naive(num)
+
+    def naive(self, n):
+        result = n
         while result > 9:
             result = sum(int(s) for s in str(result))
         return result
+
+    def congruence_formula(self, n):
+        return 1 + ((n - 1) % 9)
 
 def main():
     nums = [38]
