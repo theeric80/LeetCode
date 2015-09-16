fc1f9a5d6ae1051cd27f206039b174d6ecb4721f
Eric Tsai <chunchieh@gmail.com>
2015-09-16 08:37:08 +0800

    Refine solution: 27 Remove Element

--- remove_element.py
+++ remove_element.py
@@ -12,18 +12,19 @@ class Solution(object):
                 continue
             # swap two elements nums[i] and nums[j]
             # nums[j] is the last element that it's values doesn't equal to val
-            while j > i:
-                j -= 1
-                if nums[j] == val:
-                    continue
-                nums[i], nums[j] = nums[j], nums[i]
+            if i >= j:
                 break
+            j -= 1
+            while j > i and nums[j] == val:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
         return j
 
 def main():
-    inputs = [1, 1, 2, 2, 1, 3, 4, 1, 5]
-    result = Solution().removeElement(inputs, 1)
-    print result, inputs
+    inputs = [([1, 1, 2, 2, 3, 4, 5], 1), ([4, 5], 5), ([3, 3], 3), ([1], 1)]
+    for nums, val in inputs:
+        result = Solution().removeElement(nums, val)
+        print result, nums
 
 if __name__ == '__main__':
     main()
