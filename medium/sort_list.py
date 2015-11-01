586758c0b4fd77f3b3cdd6b1f1099ef4e39602da
Eric Tsai <chunchieh@gmail.com>
2015-11-01 02:11:50 +0000

    Refine solution: 148 Sort List

--- medium/sort_list.py
+++ medium/sort_list.py
@@ -10,12 +10,51 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        return self.sortList_0(head)
+        return self.sortList_1(head)
 
     def sortList_1(self, head):
-        # TODO
+        # top-down mergesort
         # O(nlogn)
-        return head
+        if not head or not head.next:
+            return head
+
+        prev, slow, fast = None, head, head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next
+        prev.next = None
+
+        l1 = self.sortList_1(head)
+        l2 = self.sortList_1(slow)
+        return self.mergeList_1(l1, l2)
+
+    def mergeList_1(self, l1, l2):
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+
+        """
+        if l1.val < l2.val:
+            l1.next = self.mergeList_1(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeList_1(l1, l2.next)
+            return l2
+        """
+        head = ListNode(0)
+        end = head
+        while l1 and l2:
+            if l1.val < l2.val:
+                end.next = l1
+                l1 = l1.next
+            else:
+                end.next = l2
+                l2 = l2.next
+            end = end.next
+        end.next = l1 if l1 else l2
+        return head.next
 
     def sortList_0(self, head):
         # O(n^2)
