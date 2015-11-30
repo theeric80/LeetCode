52533e030497437ce73b242289e0c7878fa210e7
Eric Tsai <chunchieh@gmail.com>
2015-11-30 07:05:32 +0000

    Refine solution: 3 Longest Substring Without Repeating Characters

--- medium/longest_substring_without_repeating_characters.py
+++ medium/longest_substring_without_repeating_characters.py
@@ -6,8 +6,21 @@ class Solution(object):
         :rtype: int
         """
         if s:
-            return self.lengthOfLongestSubstring_0(s)
+            return self.lengthOfLongestSubstring_1(s)
         return 0
+
+    def lengthOfLongestSubstring_1(self, s):
+        result = 1
+        n = len(s)
+        j = 0
+        dp = {s[0]: 0}
+        for i in xrange(1, n):
+            c = s[i]
+            if c in dp:
+                j = max(j, dp[c]+1)
+            dp[c] = i
+            result = max(result, i-j+1)
+        return result
 
     def lengthOfLongestSubstring_0(self, s):
         result = 1
@@ -26,11 +39,11 @@ class Solution(object):
         return result
 
 def main():
-    inputs = ['abcabcbb', 'bbbbb', '', 'c', 'dvdf']
-    inputs = ['dvdf']
+    inputs = ['abcabcbb', 'bbbbb', '', 'c']
+    inputs += ['dvdf']
     for s in inputs:
         result = Solution().lengthOfLongestSubstring(s)
-        print result
+        print '{0}: {1}'.format(s, result)
 
 if __name__ == '__main__':
     main()
