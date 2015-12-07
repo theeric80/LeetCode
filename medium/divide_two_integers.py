0c50ee2405b45f8e3f38ae4e6dd039ee192f1e49
Eric Tsai <chunchieh@gmail.com>
2015-12-07 21:54:47 +0800

    Refine solution: 29 Divide Two Integers

--- medium/divide_two_integers.py
+++ medium/divide_two_integers.py
@@ -6,18 +6,30 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        import sys
-        if dividend > sys.maxint: return sys.maxint
+        minint, maxint = -2147483648, 2147483647
+        # overflow
+        if ((divisor == 0) or 
+            (dividend == minint and divisor == -1)):
+            return maxint
+        elif divisor == 1:
+            return dividend
+
         result = 0
-        remainder = dividend
-        while remainder >= divisor:
-            remainder -= divisor
-            result += 1
-        return result
+        sign = -1 if (dividend > 0) ^ (divisor > 0) else 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        while dividend >= divisor:
+            r, m = divisor, 1
+            while dividend >= (r<<1):
+                r <<= 1
+                m <<= 1
+            dividend -= r
+            result += m
+        return result if sign > 0 else -result
 
 def main():
-    inputs = [(11,2), (0,1), (1,1)]
-    inputs += [(1,-1)]
+    inputs = [(21,2), (0,1), (1,1)]
+    inputs += [(1,-1), (-2147483648, -1)]
     for dividend, divisor in inputs:
         result = Solution().divide(dividend, divisor)
         print '{0} / {1} = {2}'.format(dividend, divisor, result)
