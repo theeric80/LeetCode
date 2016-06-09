ce4fcd66bed5a9839bc94b615afbb414d443f7a1
Eric Tsai <chunchieh@gmail.com>
2016-06-09 22:11:34 +0800

    Add solution: 337 House Robber III

---
+++ medium/house_robber_iii.py
@@ -0,0 +1,54 @@
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        result = (0, 0)
+        if root:
+            result = self.dfs(root)
+        return max(result)
+
+    def dfs(self, node):
+        if not node: return (0, 0)
+        l = self.dfs(node.left)
+        r = self.dfs(node.right)
+        ret0 = node.val + l[1] + r[1]
+        ret1 = max(l) + max(r)
+        return (ret0, ret1)
+
+    def level_order(self, root):
+        # Wrong answer
+        result = [0, 0]
+        l, curr, temp = 0, [root], []
+        while curr:
+            while curr:
+                node = curr.pop(0)
+                result[l] += node.val
+                if node.left:  temp.append(node.left)
+                if node.right: temp.append(node.right)
+            curr, temp = temp, []
+            l = (l+1) % 2
+        return result
+
+def main():
+    import sys
+    from os.path import join, abspath
+    sys.path.append(join('..', 'common'))
+
+    from utils import build_oj_binary_tree
+    root = build_oj_binary_tree([3,2,3,'#',3,'#',1])
+    result = Solution().rob(root)
+    print result
+
+    root = build_oj_binary_tree([3,4,5,1,3,'#',1])
+    result = Solution().rob(root)
+    print result
+
+    root = build_oj_binary_tree([4,1,'#',2,'#',3])
+    result = Solution().rob(root)
+    print result
+
+if __name__ == '__main__':
+    main()
