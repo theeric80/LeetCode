22a3ed42faf0ceae2e2238987f4d2c776036faae
Eric Tsai <chunchieh@gmail.com>
2016-06-15 00:34:23 +0000

    Refine solution: 357 Count Numbers with Unique Digits

--- medium/count_numbers_with_unique_digits.py
+++ medium/count_numbers_with_unique_digits.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """
         used = [False] * 10
         last = 0
         return self.countNumbersWithUniqueDigits_0(n, used, last) + 1
+        """
+        return self.countNumbersWithUniqueDigits_1(n)
 
     def countNumbersWithUniqueDigits_0(self, n, used, last):
         # backtracing
@@ -22,8 +25,24 @@ class Solution(object):
                 used[i] = False
         return count
 
+    def countNumbersWithUniqueDigits_1(self, n):
+        # dynamic programming
+        """
+        i=0: 1
+        i=1: 9
+        i=2: 9*9
+        i=3: 9*9*8
+        ...
+        i=n: 9*9*8*...*(11-i)   , i<=10
+        """
+        sz = min(n, 10)+1
+        m = [1] * sz
+        for i in xrange(1, sz):
+            m[i] = m[i-1] + 9 * reduce(lambda a, b: a*b, [1]+range(9, 10-i, -1))
+        return m[-1]
+
 def main():
-    inputs = [0,1,2,3]
+    inputs = [0,1,2,3,5,10,11]
     for n in inputs:
         result = Solution().countNumbersWithUniqueDigits(n)
         print 'n={}, result={}'.format(n, result)
