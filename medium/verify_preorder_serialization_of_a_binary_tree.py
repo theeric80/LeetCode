e0043959c1eb0cfbafb19bf3a512e993aef254e4
Eric Tsai <chunchieh@gmail.com>
2016-06-16 02:16:06 +0000

    Refine solution: 331 Verify Preorder Serialization of a Binary Tree

--- medium/verify_preorder_serialization_of_a_binary_tree.py
+++ medium/verify_preorder_serialization_of_a_binary_tree.py
@@ -6,7 +6,10 @@ class Solution(object):
         :rtype: bool
         """
         nodes = preorder.split(',')
+        """
         return self.construct(nodes, 0) == len(nodes)-1
+        """
+        return self.isValidSerialization_0(nodes)
 
     def construct(self, preorder, i):
         if i >= len(preorder):
@@ -17,6 +20,20 @@ class Solution(object):
         l = self.construct(preorder, i+1)  # l: the last node of left subtree
         r = self.construct(preorder, l+1)  # r: the last node of right subtree
         return r
+
+    def isValidSerialization_0(self, preorder):
+        # use stack
+        # https://leetcode.com/discuss/83825/simple-python-solution-using-stack-with-explanation
+        s = []
+        for n in preorder:
+            s.append(n)
+            while len(s) >= 2 and s[-1] == '#' and s[-2] == '#':
+                s = s[:-2]
+                if len(s) == 0:
+                    return False
+                s.pop()
+                s.append('#')
+        return len(s) == 1 and s[-1] == '#'
 
 def main():
     inputs= ['9,3,4,#,#,1,#,#,2,#,6,#,#', '9,#,#,1', '1,#']
