e05321d8c2143d35279136d3999e1be1e7005690
Eric Tsai <chunchieh@gmail.com>
2016-06-30 05:32:44 +0000

    Refine solution: 310 Minimum Height Trees

--- medium/minimum_height_trees.py
+++ medium/minimum_height_trees.py
@@ -24,36 +24,63 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
-        return self.findMinHeightTrees_0(n, edges)
+        return self.findMinHeightTrees_1(n, edges)
 
     def findMinHeightTrees_0(self, n, edges):
         min_h = n
         result = []
         G = Graph(n, edges)
         for v in xrange(n):
-            h = self.bfs(G, v)
+            h = len(self.bfs(G, v))
             if h < min_h:
                 result, min_h = [v], h
             elif h == min_h:
                 result.append(v)
         return result
 
-    def bfs(self, G, v):
+    def findMinHeightTrees_1(self, n, edges):
+        if n <= 0: return []
+        G = Graph(n, edges)
+
+        # find the longest path in G
+        v = self.bfs(G, 0)[-1]
+        path = self.bfs(G, v)
+
+        m = len(path)
+        return path[(m-1)/2:m/2+1]
+
+    def bfs(self, G, s):
+        from operator import itemgetter
+
         n = G.V()
         marked = [False] * n
-        length = [0] * n
+        edge_to = [s] * n
+        dist_to = [0] * n
 
-        marked[v] = True
-        q = [v]
+        marked[s] = True
+        q = [s]
 
         while q:
             v = q.pop(0)
             for w in G.adj(v):
                 if not marked[w]:
                     marked[w] = True
-                    length[w] = length[v] + 1
+                    edge_to[w] = v
+                    dist_to[w] = dist_to[v] + 1
                     q.append(w)
-        return max(length)
+
+        w, d = max(enumerate(dist_to), key=itemgetter(1))
+        return self.path(s, w, edge_to)
+
+    def path(self, v, w, edge_to):
+        result = []
+        x = w
+        while x != v:
+            result.append(x)
+            x = edge_to[x]
+        result.append(v)
+        result.reverse()
+        return result
 
 def main():
     n, edges = 0, []
